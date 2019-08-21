a107394a07cf3be8c69fcec0bbfe0b18ca2a7126
Mukesh <mrdmukesh@gmail.com>
2019-08-21 20:14:25 +0530

    First
    First

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+           MRD Airbnb 2019. 
+        </p>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/HomeContent.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function HomeContent() {
+  return (
+    <section className="container">
+        <div className="columns features">
+            <div className="column is-4">
+                <div className="card is-shady">
+                    <div className="card-content">
+                        <div className="content">
+                            <h4>24/7 customer support</h4>
+                            <p>Day or night, we’re here for you. Talk to our support team from anywhere in the world, any hour of day.</p>
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="column is-4">
+                <div className="card is-shady">
+                    <div className="card-content">
+                        <div className="content">
+                            <h4>Global hospitality standards</h4>
+                            <p>Guests review their hosts after each stay. All hosts must maintain a minimum rating and our hospitality standards to be on Airbnb.</p>
+                            
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="column is-4">
+                <div className="card is-shady">
+                     <div className="card-content">
+                        <div className="content">
+                            <h4>5-star hosts</h4>
+                            <p>From fresh-pressed sheets to tips on where to get the best brunch, our hosts are full of local hospitality.</p>
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
